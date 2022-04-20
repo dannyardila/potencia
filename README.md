3cbf8168b73d87fadca55226837e59d7efcf579b
dannyardia <dannyardila059@gmail.com>
2022-04-20 08:51:42 -0500

    Desarrollo: programa terminado en python. dannyardila

---
+++ README.md
@@ -0,0 +1,6 @@
+# Programa No.2
+
+### Programa para calacular x a la y. 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
+
---
+++ potencia.py
@@ -0,0 +1,16 @@
+""" Programa No. 2:
+Programa para calcular x a la y"""
+
+print("-----------------------")
+print("-----potencia a la y------")
+print("-----------")
+
+#input 
+x= int(input("Digite el valor de x:") )
+y= int (input("Digite el valor de y ") )
+
+#procesing
+z=x**y
+
+#output
+print(str(x)+ " a la "+ str(y)+ " es igual a " + str (z))
